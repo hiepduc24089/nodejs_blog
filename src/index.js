a5add754f006cdd648f2bb95cc4d519628298b8a
hiepduc24089 <146322141+hiepduc24089@users.noreply.github.com>
2025-01-07 17:34:08 +0700

    add prettier

--- src/index.js
+++ src/index.js
@@ -1,37 +1,42 @@
-import express from 'express'
-import morgan from 'morgan'
-import {engine} from 'express-handlebars'
-import path, { dirname } from 'path'
-import { fileURLToPath } from 'url'
-import route from './routes/index.js'
+import express from 'express';
+import morgan from 'morgan';
+import { engine } from 'express-handlebars';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import route from './routes/index.js';
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 //Đường dẫn đến thư mục chứa file tĩnh
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.urlencoded({
-  extended: true
-}));
+app.use(
+  express.urlencoded({
+    extended: true,
+  }),
+);
 app.use(express.json());
 
 //HTTP logger
 app.use(morgan('combined'));
 
 // Template engine
-app.engine('hbs', engine({
-  extname: '.hbs'
-}));
+app.engine(
+  'hbs',
+  engine({
+    extname: '.hbs',
+  }),
+);
 app.set('view engine', 'hbs'); //Sử dụng handlebars làm view engine
-app.set('views', path.join(__dirname, 'resources/views')) //Đường dẫn đến thư mục chứa view
+app.set('views', path.join(__dirname, 'resources/views')); //Đường dẫn đến thư mục chứa view
 
 //Route init
 route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
